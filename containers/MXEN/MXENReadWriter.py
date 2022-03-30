693363a0397a7707dce8a43092ba551086aa74a6
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-30 21:10:12 +0100

    Implement initial MXEN readwriter logic

--- containers/MXEN/MXENReadWriter.py
+++ containers/MXEN/MXENReadWriter.py
@@ -3,5 +3,12 @@ from pyValkLib.serialisation.ValkyriaBaseRW import ValkyriaBaseRW32BH
 class MXENReadWriter(ValkyriaBaseRW32BH):
     FILETYPE = "MXEN"
     
+    def __init__(self, containers, endianness=None):
+        super().__init__(containers, endianness)
+        
+        self.MXEC = containers["MXEC"](containers, endianness)
+        
+        self.subcontainers.extend([self.MXEC])
+    
     def read_write_contents(self):
-        pass
+        self.assert_equal("flags", 0x10000005, self.header, lambda x: hex(x))
