ed2b246e4e5cc0432edf4e320b020e34403ac7e7
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-04 07:40:49 +0100

    Register MXEC and POF0

--- containers/__init__.py
+++ containers/__init__.py
@@ -1,7 +1,9 @@
 from .MXEN.MXENReadWriter import MXENReadWriter
 from .MXEN.MXEC.MXECReadWriter import MXECReadWriter
+from .POF0.POF0ReadWriter import POF0ReadWriter
 
 containers = {
     "MXEN": MXENReadWriter,
-    "MXEC": MXECReadWriter
+    "MXEC": MXECReadWriter,
+    "POF0": POF0ReadWriter
 }
