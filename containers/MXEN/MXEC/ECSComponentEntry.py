84884c280db14d1ca6c81e58bd2c2227891b4767
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-04 21:09:11 +0100

    Fix ComponentEntry readwrites

--- containers/MXEN/MXEC/ECSComponentEntry.py
+++ containers/MXEN/MXEC/ECSComponentEntry.py
@@ -1,17 +1,18 @@
 from pyValkLib.serialisation.BaseRW import BaseRW
+from pyValkLib.containers.MXEN.MXEC.StructureList import data_types
 
 
 class ComponentEntry(BaseRW):
-    __slots__ = ("ID", "name_offset", "data_size", "data_offset")
+    __slots__ = ("ID", "name_offset", "data_size", "data_offset", "data")
 
     def __init__(self, endianness):
         super().__init__(endianness)
         self.ID = 0
-        self.offset_1 = 0
+        self.name_offset = 0
         self.data_size = 0
         self.data_offset = 0
         
-        self.data_2 = None
+        self.data = None
         
 
     def read_write(self):
@@ -20,6 +21,7 @@ class ComponentEntry(BaseRW):
         self.rw_var("data_size", "I")
         self.rw_var("data_offset", "I")
         
-    def rw_data_2(self, lookup_name):
-        self.rw_readwriter(self.data_2)
-        # getattr(self.data_2, self.rw_method)(self.bytestream)
+    def rw_data(self, lookup_name):
+        if self.rw_method == "read":
+            self.data = data_types[lookup_name]()
+        self.rw_readwriter(self.data)
