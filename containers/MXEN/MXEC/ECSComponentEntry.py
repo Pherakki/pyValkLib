9dd017bc1161af656ba605d299fe97dd8a40a71a
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-04-04 07:39:43 +0100

    Give the offsets names

--- containers/MXEN/MXEC/ECSComponentEntry.py
+++ containers/MXEN/MXEC/ECSComponentEntry.py
@@ -2,23 +2,23 @@ from pyValkLib.serialisation.BaseRW import BaseRW
 
 
 class ComponentEntry(BaseRW):
-    __slots__ = ("ID", "offset_1", "data_size", "offset_2")
+    __slots__ = ("ID", "name_offset", "data_size", "data_offset")
 
     def __init__(self, endianness):
         super().__init__(endianness)
         self.ID = 0
         self.offset_1 = 0
         self.data_size = 0
-        self.offset_2 = 0
+        self.data_offset = 0
         
         self.data_2 = None
         
 
     def read_write(self):
         self.rw_var("ID", "I")
-        self.rw_var("offset_1", "I")
+        self.rw_var("name_offset", "I")
         self.rw_var("data_size", "I")
-        self.rw_var("offset_2", "I")
+        self.rw_var("data_offset", "I")
         
     def rw_data_2(self, lookup_name):
         self.rw_readwriter(self.data_2)
