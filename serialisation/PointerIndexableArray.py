93e979c97baac9a9007d4a8c314b41e2e248bde5
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-04 20:41:32 +0100

    Add basic PointerIndexableArray: yet to define read_write

---
+++ serialisation/PointerIndexableArray.py
@@ -0,0 +1,22 @@
+from pyValkLib.serialisation.Serializable import Serializable
+
+
+class PointerIndexableArray(Serializable):
+    def __init__(self, context):
+        self.context.anchor_pos = context.anchor_pos
+        self.context.endianness = context.endianness
+        
+        self.data = []
+        self.ptr_to_idx = {}
+        self.idx_to_ptr = []
+        
+    def at_ptr(self, ptr):
+        return self.data[self.ptr_to_idx[ptr]]
+    
+    def at_idx(self, idx):
+        return self.data[idx]
+    
+    def __iter__(self):
+        for elem in self.data:
+            yield elem
+    
