9e2c07f9b3f1f662d6fb5f581db271e2a5faeb7b
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-04 20:56:02 +0100

    Convert EntryTable to inherit from Serializable

--- containers/MXEN/MXEC/EntryTable.py
+++ containers/MXEN/MXEC/EntryTable.py
@@ -1,50 +1,50 @@
-from pyValkLib.serialisation.BaseRW import BaseRW
-from pyValkLib.serialisation.ValkyriaBaseRW import PointerIndexableArray
+from pyValkLib.serialisation.Serializable import Serializable
+from pyValkLib.serialisation.PointerIndexableArray import PointerIndexableArray
 
-class EntryTable(BaseRW):
+class EntryTable(Serializable):
     def __init__(self, entry_cls, endianness):
         super().__init__(endianness)
         self.entry_cls = entry_cls
         self.entries = PointerIndexableArray()
     
-    def read_write(self):
-        self.rw_var("padding_0x00", "I")
-        self.rw_var("entry_count", "I")
-        self.rw_var("entry_ptr", "I")
-        self.rw_var("padding_0x0C", "I")
+    def read_write(self, rw):
+        self.padding_0x00 = rw.rw_uint32(self.padding_0x00)
+        self.entry_count  = rw.rw_uint32(self.entry_count)
+        self.entry_ptr    = rw.rw_uint32(self.entry_ptr)
+        self.padding_0x0C = rw.rw_uint32(self.padding_0x0C)
         
-        self.rw_var("padding_0x10", "I")
-        self.rw_var("padding_0x14", "I")
-        self.rw_var("padding_0x18", "I")
-        self.rw_var("padding_0x1C", "I")
+        self.padding_0x10 = rw.rw_uint32(self.padding_0x10)
+        self.padding_0x14 = rw.rw_uint32(self.padding_0x14)
+        self.padding_0x18 = rw.rw_uint32(self.padding_0x18)
+        self.padding_0x1C = rw.rw_uint32(self.padding_0x1C)
         
-        self.rw_var("padding_0x20", "I")
-        self.rw_var("padding_0x24", "I")
-        self.rw_var("padding_0x28", "I")
-        self.rw_var("padding_0x2C", "I")
+        self.padding_0x20 = rw.rw_uint32(self.padding_0x20)
+        self.padding_0x24 = rw.rw_uint32(self.padding_0x24)
+        self.padding_0x28 = rw.rw_uint32(self.padding_0x28)
+        self.padding_0x2C = rw.rw_uint32(self.padding_0x2C)
         
-        self.rw_var("padding_0x30", "I")
-        self.rw_var("padding_0x34", "I")
-        self.rw_var("padding_0x38", "I")
-        self.rw_var("padding_0x3C", "I")
+        self.padding_0x30 = rw.rw_uint32(self.padding_0x30)
+        self.padding_0x34 = rw.rw_uint32(self.padding_0x34)
+        self.padding_0x38 = rw.rw_uint32(self.padding_0x38)
+        self.padding_0x3C = rw.rw_uint32(self.padding_0x3C)
                 
-        self.assert_equal("padding_0x00", 0)
-        self.assert_equal("padding_0x0C", 0)
+        rw.assert_is_zero(self.padding_0x00)
+        rw.assert_is_zero(self.padding_0x0C)
         
-        self.assert_equal("padding_0x10", 0)
-        self.assert_equal("padding_0x14", 0)
-        self.assert_equal("padding_0x18", 0)
-        self.assert_equal("padding_0x1C", 0)
+        rw.assert_is_zero(self.padding_0x10)
+        rw.assert_is_zero(self.padding_0x14)
+        rw.assert_is_zero(self.padding_0x18)
+        rw.assert_is_zero(self.padding_0x1C)
         
-        self.assert_equal("padding_0x20", 0)
-        self.assert_equal("padding_0x24", 0)
-        self.assert_equal("padding_0x28", 0)
-        self.assert_equal("padding_0x2C", 0)
+        rw.assert_is_zero(self.padding_0x20)
+        rw.assert_is_zero(self.padding_0x24)
+        rw.assert_is_zero(self.padding_0x28)
+        rw.assert_is_zero(self.padding_0x2C)
         
-        self.assert_equal("padding_0x30", 0)
-        self.assert_equal("padding_0x34", 0)
-        self.assert_equal("padding_0x38", 0)
-        self.assert_equal("padding_0x3C", 0)
+        rw.assert_is_zero(self.padding_0x30)
+        rw.assert_is_zero(self.padding_0x34)
+        rw.assert_is_zero(self.padding_0x38)
+        rw.assert_is_zero(self.padding_0x3C)
         
         if self.rw_method == "read":
             self.entries.data = [self.entry_cls(self.endianness) for _ in range(self.entry_count)]
