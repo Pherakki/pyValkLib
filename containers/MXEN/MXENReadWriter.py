e5917fa1e8715ff420b1753ecaa3ef05a2fab24d
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-04 19:24:53 +0100

    Convert MXEN to new serialiser

--- containers/MXEN/MXENReadWriter.py
+++ containers/MXEN/MXENReadWriter.py
@@ -1,17 +1,17 @@
-from pyValkLib.serialisation.ValkyriaBaseRW import ValkyriaBaseRW32BH
+from pyValkLib.serialisation.ValkSerializable import ValkSerializable32BH
 
-class MXENReadWriter(ValkyriaBaseRW32BH):
+class MXENReadWriter(ValkSerializable32BH):
     FILETYPE = "MXEN"
     
     def __init__(self, containers, endianness=None):
         super().__init__(containers, endianness)
         
-        self.MXEC = containers["MXEC"](containers, endianness)
+        #self.MXEC = containers["MXEC"](containers, endianness)
         
-        self.subcontainers.extend([self.MXEC])
+        #self.subcontainers.extend([self.MXEC])
     
-    def read_write_contents(self):
-        self.assert_equal("flags", 0x10000005, self.header, lambda x: hex(x))
+    def read_write_contents(self, rw):
+        rw.assert_equal(self.header.flags, 0x10000005, lambda x: hex(x))
         
     def __repr__(self):
         return f"MXEN Object [{self.header.depth}] [0x{self.header.flags:0>8x}]: Contains MXEC."
--- containers/__init__.py
+++ containers/__init__.py
@@ -1,15 +1,31 @@
-from .CCRS.CCRSReadWriter import CCRSReadWriter
-from .ENRS.ENRSReadWriter import ENRSHandler
-from .EOFC.EOFCReadWriter import EOFCReadWriter
+# from .CCRS.CCRSReadWriter import CCRSReadWriter
+# from .ENRS.ENRSReadWriter import ENRSHandler
+# from .EOFC.EOFCReadWriter import EOFCReadWriter
+# from .IZCA.IZCAReadWriter import IZCAReadWriter
+# from .IZCA.CCOL.CCOLReadWriter import CCOLReadWriter
+# from .IZCA.HMDL.HMDLReadWriter import HMDLReadWriter
+# from .IZCA.HSHP.HSHPReadWriter import HSHPReadWriter
+# from .IZCA.HTEX.HTEXReadWriter import HTEXReadWriter
+# from .IZCA.MLX0.MLX0ReadWriter import MLX0ReadWriter
+# from .IZCA.PJNT.PJNTReadWriter import PJNTReadWriter
+# from .IZCA.PACT.PACTReadWriter import PACTReadWriter
 from .MXEN.MXENReadWriter import MXENReadWriter
-from .MXEN.MXEC.MXECReadWriter import MXECReadWriter
-from .POF0.POF0ReadWriter import POF0Handler
+# from .MXEN.MXEC.MXECReadWriter import MXECReadWriter
+# from .POF0.POF0ReadWriter import POF0Handler
 
 containers = {
-    "CCRS": CCRSReadWriter,
-    "ENRS": ENRSHandler,
-    "EOFC": EOFCReadWriter,
+    # "CCOL": CCOLReadWriter,
+    # "CCRS": CCRSReadWriter,
+    # "ENRS": ENRSHandler,
+    # "EOFC": EOFCReadWriter,
+    # "HMDL": HMDLReadWriter,
+    # "HSHP": HSHPReadWriter,
+    # "HTEX": HTEXReadWriter,
+    # "IZCA": IZCAReadWriter,
+    # "MLX0": MLX0ReadWriter,
     "MXEN": MXENReadWriter,
-    "MXEC": MXECReadWriter,
-    "POF0": POF0Handler
+    # "MXEC": MXECReadWriter,
+    # "PACT": PACTReadWriter,
+    # "PJNT": PJNTReadWriter,
+    # "POF0": POF0Handler
 }
