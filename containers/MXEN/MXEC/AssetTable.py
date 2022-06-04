81400078a1c8bcb5d4a9904894dd66b7843dd7bd
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-04 23:22:01 +0100

    Inherit AssetTable from Serializable

--- containers/MXEN/MXEC/AssetTable.py
+++ containers/MXEN/MXEC/AssetTable.py
@@ -1,9 +1,9 @@
-from pyValkLib.serialisation.BaseRW import BaseRW
+from pyValkLib.serialisation.Serializable import Serializable
 from .AssetEntry import AssetEntry
-from pyValkLib.serialisation.ValkyriaBaseRW import PointerIndexableArray
+from pyValkLib.serialisation.PointerIndexableArray import PointerIndexableArray
 
 
-class AssetTable(BaseRW):
+class AssetTable(Serializable):
     def __init__(self, endianness):
         super().__init__(endianness)
         self.padding_0x00 = None
@@ -23,25 +23,23 @@ class AssetTable(BaseRW):
         if self.rw_method == 'read':
             self.init_structs()
                 
-    def rw_header(self):
-        self.rw_var("padding_0x00", "I")
-        self.rw_var("asset_reference_count", "I")
-        self.rw_var("asset_references_offset", "I")
-        self.rw_var("asset_use_count", "I")
-        self.rw_var("asset_use_offset", "I")
-        self.rw_var("padding_0x14", "I")
-        self.rw_var("padding_0x18", "I")
-        self.rw_var("padding_0x1C", "I")
+    def rw_header(self, rw):
+        self.padding_0x00            = rw.rw_uint32(self.padding_0x00)
+        self.asset_reference_count   = rw.rw_uint32(self.asset_reference_count)
+        self.asset_references_offset = rw.rw_uint32(self.asset_references_offset)
+        self.asset_use_count         = rw.rw_uint32(self.asset_use_count)
+        self.asset_use_offset        = rw.rw_uint32(self.asset_use_offset)
+        self.padding_0x14            = rw.rw_uint32(self.padding_0x14)
+        self.padding_0x18            = rw.rw_uint32(self.padding_0x18)
+        self.padding_0x1C            = rw.rw_uint32(self.padding_0x1C)
         
-        self.assert_is_zero("padding_0x00")
-        self.assert_is_zero("padding_0x14")
-        self.assert_is_zero("padding_0x18")
-        self.assert_is_zero("padding_0x1C")
+        rw.assert_is_zero(self.padding_0x00)
+        rw.assert_is_zero(self.padding_0x14)
+        rw.assert_is_zero(self.padding_0x18)
+        rw.assert_is_zero(self.padding_0x1C)
         
     def init_structs(self):
         self.entries.data = [AssetEntry(self.endianness) for _ in range(self.asset_reference_count)]
         
-    def rw_asset_slot_offsets(self):
-        self.rw_varlist("asset_slot_offsets", 'I', self.asset_use_count)
-
-        
+    def rw_asset_slot_offsets(self, rw):
+        self.asset_slot_offsets = rw.rw_uint32s(self.asset_slot_offsets, self.asset_use_count)
