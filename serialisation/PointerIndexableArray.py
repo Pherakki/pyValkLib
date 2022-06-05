7e762f277d3492784aa130afdf67f00b9ed51217
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-05 13:30:48 +0100

    Add read_write method for PointerIndexableArray

--- serialisation/PointerIndexableArray.py
+++ serialisation/PointerIndexableArray.py
@@ -2,13 +2,14 @@ from pyValkLib.serialisation.Serializable import Serializable
 
 
 class PointerIndexableArray(Serializable):
-    def __init__(self):
+    def __init__(self, context):
+        super().__init__(context)
         #self.context.anchor_pos = context.anchor_pos
         #self.context.endianness = context.endianness
         
         self.data = []
         self.ptr_to_idx = {}
-        self.idx_to_ptr = []
+        self.idx_to_ptr = {}
         
     def at_ptr(self, ptr):
         return self.data[self.ptr_to_idx[ptr]]
@@ -19,4 +20,17 @@ class PointerIndexableArray(Serializable):
     def __iter__(self):
         for elem in self.data:
             yield elem
+    def read_write(self, rw):
+        for i, elem in enumerate(self.data):
+            if i in self.idx_to_ptr:
+                rw.assert_local_file_pointer_at("Start of Array Entry", self.idx_to_ptr[i])
+            curpos = rw.local_tell()
+            self.ptr_to_idx[curpos] = i
+            self.idx_to_ptr[i] = curpos
+
+            self.rw_element(rw, i)
+
+    def rw_element(self, rw, idx):
+        rw.rw_obj(self.data[idx])
     
+class PointerIndexableArrayInt8(Serializable):
