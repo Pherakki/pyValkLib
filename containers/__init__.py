944d9ad3370c51e09c752dc48476bddf1c04d16c
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-05 15:45:20 +0100

    Re-enable POF0

--- containers/__init__.py
+++ containers/__init__.py
@@ -11,7 +11,7 @@
 # from .IZCA.PACT.PACTReadWriter import PACTReadWriter
 from .MXEN.MXENReadWriter import MXENReadWriter
 from .MXEN.MXEC.MXECReadWriter import MXECReadWriter
-# from .POF0.POF0ReadWriter import POF0Handler
+from .POF0.POF0ReadWriter import POF0Handler
 
 containers = {
     # "CCOL": CCOLReadWriter,
@@ -27,5 +27,5 @@ containers = {
     "MXEC": MXECReadWriter,
     # "PACT": PACTReadWriter,
     # "PJNT": PJNTReadWriter,
-    # "POF0": POF0Handler
+    "POF0": POF0Handler
 }
