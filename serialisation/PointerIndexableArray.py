7fd9e3b2726fba72c9d063dcc3b1782d0828dc1d
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-05 13:31:12 +0100

    Add derived classes of PointerIndexableArray for primitive types

--- serialisation/PointerIndexableArray.py
+++ serialisation/PointerIndexableArray.py
@@ -20,6 +20,7 @@ class PointerIndexableArray(Serializable):
     def __iter__(self):
         for elem in self.data:
             yield elem
+    
     def read_write(self, rw):
         for i, elem in enumerate(self.data):
             if i in self.idx_to_ptr:
@@ -34,3 +35,41 @@ class PointerIndexableArray(Serializable):
         rw.rw_obj(self.data[idx])
     
 class PointerIndexableArrayInt8(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_int8(self.data[idx])
+    
+class PointerIndexableArrayUint8(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_uint8(self.data[idx])
+    
+class PointerIndexableArrayInt16(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_int16(self.data[idx])
+    
+class PointerIndexableArrayUint16(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_uint16(self.data[idx])
+    
+class PointerIndexableArrayInt32(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_int32(self.data[idx])
+    
+class PointerIndexableArrayUint32(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_uint32(self.data[idx])
+    
+class PointerIndexableArrayInt64(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_int64(self.data[idx])
+    
+class PointerIndexableArrayUint64(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_uint64(self.data[idx])
+    
+class PointerIndexableArrayFloat16(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_float16(self.data[idx])
+    
+class PointerIndexableArrayFloat32(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_float64(self.data[idx])
+    
+class PointerIndexableArrayFloat64(Serializable):
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_float64(self.data[idx])
+    
+class PointerIndexableArrayCStr(Serializable):
+    def __init__(self, context, encoding='ascii'):
+        super().__init__(context)
+        self.encoding = encoding
+        
+    def rw_element(self, rw, idx): self.data[idx] = rw.rw_cstr(self.data[idx], encoding=self.encoding)
