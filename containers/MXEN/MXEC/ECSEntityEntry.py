22f9aa46f93b9d057205c43cf196d96ccc273267
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-05 14:47:50 +0100

    Fix ECS Entity objects

--- containers/MXEN/MXEC/ECSEntityEntry.py
+++ containers/MXEN/MXEC/ECSEntityEntry.py
@@ -1,8 +1,11 @@
 from pyValkLib.serialisation.Serializable import Serializable
-from pyValkLib.serialisation.PointerIndexableArray import PointerIndexableArray
+from pyValkLib.serialisation.PointerIndexableArray import PointerIndexableArray, PointerIndexableArrayUint32
     
 class EntityEntry(Serializable):
-    __slots__ = ("ID", "name_offset", "count", "data_offset")
+    __slots__ = ("ID", "name_offset", "count", "data_offset", "data", "component_type",
+                 "padding_0x10", "padding_0x14", "controller_entity_id", "padding_0x1C",
+                 "padding_0x20", "padding_0x24", "unknown_0x28", "unknown_0x2C",
+                 "padding_0x30", "padding_0x34", "padding_0x38", "padding_0x3C")
 
     def __init__(self, endianness):
         super().__init__(endianness)
@@ -11,7 +14,23 @@ class EntityEntry(Serializable):
         self.count = 0
         self.data_offset = 0
         
+        self.padding_0x10          = 0
+        self.padding_0x14          = 0
+        self.controller_entity_id  = 0
+        self.padding_0x1C          = 0
+        
+        self.padding_0x20          = 0
+        self.padding_0x24          = 0
+        self.unknown_0x28          = 0
+        self.unknown_0x2C          = 0
+        
+        self.padding_0x30          = 0
+        self.padding_0x34          = 0
+        self.padding_0x38          = 0
+        self.padding_0x3C          = 0
+        
         self.data = None
+        self.component_type = None
         
 
     def read_write(self, rw):
@@ -32,8 +51,8 @@ class EntityEntry(Serializable):
         
         self.padding_0x30          = rw.rw_uint32(self.padding_0x30)
         self.padding_0x34          = rw.rw_uint32(self.padding_0x34)
-        self.unknown_0x38          = rw.rw_uint32(self.unknown_0x38)
-        self.unknown_0x3C          = rw.rw_uint32(self.unknown_0x3C)
+        self.padding_0x38          = rw.rw_uint32(self.padding_0x38)
+        self.padding_0x3C          = rw.rw_uint32(self.padding_0x3C)
         
         rw.assert_is_zero(self.padding_0x10)
         rw.assert_is_zero(self.padding_0x14)
@@ -47,43 +66,49 @@ class EntityEntry(Serializable):
         rw.assert_is_zero(self.padding_0x38)
         rw.assert_is_zero(self.padding_0x3C)
         
-    def rw_data(self, local_tell):
-        if self.rw_method == "read":
-            self.data = EntityData(self.count, local_tell - self.bytestream.tell())
+    def rw_data(self, rw, component_type):
+        print(component_type)
+        if rw.mode() == "read":
+            self.data = EntityData(self.count, self.context)
         
-        self.rw_readwriter(self.data)
+        rw.rw_obj(self.data)
         
     def __repr__(self):
         return f"::Entity Entry:: ID: [{self.ID}], Components: [{self.count}], Controller ID: [{self.controller_entity_id}], Unk_0x28: [{self.unknown_0x28}], Unk_0x2C: [{self.unknown_0x2C}]"
        
 class EntityData(Serializable):
-    def __init__(self, count, global_to_local_offset):
+    def __init__(self, count, context):
+        super().__init__(context)
         self.count = count
-        self.global_to_local_offset = global_to_local_offset
-        self.subentries = PointerIndexableArray()
-        self.data = []
+        self.subentries = PointerIndexableArray(context)
+        self.data       = PointerIndexableArrayUint32(context)
     
     def read_write(self, rw):
-        if self.rw_method == "read":
-            self.subentries.data = [EntitySubEntry() for _ in range(self.count)]
+        if rw.mode() == "read":
+            self.subentries.data = [EntitySubEntry(self.context) for _ in range(self.count)] 
+        rw.rw_obj(self.subentries)
         
-        for i, subentry in enumerate(self.subentries.data):
-            curpos = self.bytestream.tell() + self.global_to_local_offset
-            self.subentries.ptr_to_idx[curpos] = i
-            self.subentries.idx_to_ptr.append(curpos)
-            getattr(subentry, self.rw_method)(self.bytestream)
+        if rw.mode() == "read":
+            n_to_rw = sum([subentry.count for subentry in self.subentries.data])
+            self.data.data = [0 for _ in range(n_to_rw)]
+        rw.rw_obj(self.data)
             
-        # Should make this a PointerIndexableArray
-        n_to_rw = sum([subentry.count for subentry in self.subentries.data])
-        self.rw_varlist('data', 'I', n_to_rw, endianness='>')
+        # # Should make this a PointerIndexableArray
+        # n_to_rw = sum([subentry.count for subentry in self.subentries.data])
+        # self.rw_varlist('data', 'I', n_to_rw, endianness='>')
     
     def get_data(self):
         return ( self.subentries, self.data, )
     
     
 class EntitySubEntry(Serializable):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, context):
+        super().__init__(context)
+        
+        self.name_offset = 0
+        self.count = 0
+        self.offset = 0
+        self.padding_0x0C = 0
         
     def read_write(self, rw):
         self.name_offset  = rw.rw_uint32(self.name_offset)
