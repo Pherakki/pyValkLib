1b4f032a3a5f20830fa96877e5f414400ae3395a
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-07-04 23:17:53 +0100

    Rename pathing parameters to reflect graph identities

--- containers/MXEN/MXEC/PathingEntry.py
+++ containers/MXEN/MXEC/PathingEntry.py
@@ -3,25 +3,25 @@ from pyValkLib.serialisation.PointerIndexableArray import PointerIndexableArray
 
 
 class PathingEntry(Serializable):
-    __slots__ = ("name_offset", "param_set_references", "unused_ids",
-                 "t1_count", "t1_offset", "t1_data",
-                 "t2_count", "t2_offset", "t2_data",
-                 "t3_count", "t3_offset", "t3_data",
-                 "t4_count", "t4_offset", "t4_data",
+    __slots__ = ("name_offset", "graph_edges", "unused_nodes",
+                 "node_count", "nodes_offset", "graph_nodes",
+                 "edge_count", "edges_offset", "t2_data",
+                 "graph_index_count", "graph_indices_offset", "graph_indices",
+                 "unused_node_count", "unused_nodes_offset", "t4_data",
                  "padding_0x24", "padding_0x28", "padding_0x2C",
                  "padding_0x30", "padding_0x34", "padding_0x38", "padding_0x3C")
 
     def __init__(self, context):
         super().__init__(context)
         self.name_offset = 0
-        self.t1_count  = 0
-        self.t1_offset = 0
-        self.t2_count  = 0
-        self.t2_offset = 0
-        self.t3_count  = 0
-        self.t3_offset = 0
-        self.t4_count  = 0
-        self.t4_offset = 0
+        self.node_count  = 0
+        self.nodes_offset = 0
+        self.edge_count  = 0
+        self.edges_offset = 0
+        self.graph_index_count  = 0
+        self.graph_indices_offset = 0
+        self.unused_node_count  = 0
+        self.unused_nodes_offset = 0
         
         self.padding_0x24 = 0
         self.padding_0x28 = 0
@@ -32,23 +32,23 @@ class PathingEntry(Serializable):
         self.padding_0x38 = 0
         self.padding_0x3C = 0
 
-        self.t1_data = []
-        self.param_set_references = []
-        self.t3_data = [] # Will always be 1 or 0?
-        self.unused_ids = []
+        self.graph_nodes = []
+        self.graph_edges = []
+        self.graph_indices = [] # Will always be 1 or 0?
+        self.unused_nodes = []
 
     def read_write(self, rw):
         self.name_offset  = rw.rw_pointer(self.name_offset)
-        self.t1_count     = rw.rw_uint32(self.t1_count)
-        self.t1_offset    = rw.rw_pointer(self.t1_offset)
-        self.t2_count     = rw.rw_uint32(self.t2_count)
+        self.node_count   = rw.rw_uint32(self.node_count)
+        self.nodes_offset = rw.rw_pointer(self.nodes_offset)
+        self.edge_count   = rw.rw_uint32(self.edge_count)
         
-        self.t2_offset    = rw.rw_pointer(self.t2_offset)
-        self.t3_count     = rw.rw_uint32(self.t3_count)
-        self.t3_offset    = rw.rw_pointer(self.t3_offset)
-        self.t4_count     = rw.rw_uint32(self.t4_count)
+        self.edges_offset         = rw.rw_pointer(self.edges_offset)
+        self.graph_index_count    = rw.rw_uint32(self.graph_index_count)
+        self.graph_indices_offset = rw.rw_pointer(self.graph_indices_offset)
+        self.unused_node_count    = rw.rw_uint32(self.unused_node_count)
         
-        self.t4_offset    = rw.rw_pointer(self.t4_offset)
+        self.unused_nodes_offset    = rw.rw_pointer(self.unused_nodes_offset)
         self.padding_0x24 = rw.rw_pad32(self.padding_0x24)
         self.padding_0x28 = rw.rw_pad32(self.padding_0x28)
         self.padding_0x2C = rw.rw_pad32(self.padding_0x2C)
@@ -68,150 +68,151 @@ class PathingEntry(Serializable):
         rw.assert_is_zero(self.padding_0x3C)
 
     def rw_data(self, rw):
-        if self.t1_offset:
-            rw.assert_local_file_pointer_now_at("Pathing T1 Entry", self.t1_offset)
-            rw.rw_obj_method(self, self.rw_t1_data)
+        if self.nodes_offset:
+            rw.assert_local_file_pointer_now_at("Node Headers:", self.nodes_offset)
+            rw.rw_obj_method(self, self.rw_nodes)
 
-        if self.t2_offset:
-            rw.assert_local_file_pointer_now_at("Pathing T2 Entry", self.t2_offset)
-            rw.rw_obj_method(self, self.rw_t2_data)
+        if self.edges_offset:
+            rw.assert_local_file_pointer_now_at("Edge Headers:", self.edges_offset)
+            rw.rw_obj_method(self, self.rw_edges)
 
-        for t1 in self.t1_data:
-            if t1.offset_1:
-                rw.assert_local_file_pointer_now_at("Pathing T1 Entry Data 1", t1.offset_1)
-                rw.rw_obj_method(t1, t1.rw_data_1)
-            if t1.offset_2:
-                rw.assert_local_file_pointer_now_at("Pathing T1 Entry Data 2", t1.offset_2)
-                rw.rw_obj_method(t1, t1.rw_data_2)
+        for node in self.graph_nodes:
+            if node.next_edge_list_offset:
+                rw.assert_local_file_pointer_now_at("Node Next Edges:", node.next_edge_list_offset)
+                rw.rw_obj_method(node, node.rw_next_edge_ids)
+            if node.prev_edge_list_offset:
+                rw.assert_local_file_pointer_now_at("Node Prev Edges:", node.prev_edge_list_offset)
+                rw.rw_obj_method(node, node.rw_prev_edge_ids)
 
-        for t2 in self.param_set_references:
-            if t2.offset:
-                rw.assert_local_file_pointer_now_at("Pathing T2 Entry Data", t2.offset)
-                rw.rw_obj_method(t2, t2.rw_data)
-
-        rw.align(rw.local_tell(), 0x10)
-
-        if self.t3_offset:
-            rw.assert_local_file_pointer_now_at("Pathing T3 Entry", self.t3_offset)
-            rw.rw_obj_method(self, self.rw_t3_data)
-            for t3 in self.t3_data:
-                if t3.offset_1:
-                    rw.assert_local_file_pointer_now_at("Pathing T3 Entry Data 1", t3.offset_1)
-                rw.rw_obj_method(t3, t3.rw_data_1)
-                if t3.offset_2:
-                    rw.assert_local_file_pointer_now_at("Pathing T3 Entry Data 2", t3.offset_2)
-                rw.rw_obj_method(t3, t3.rw_data_2)
-                if t3.offset_3:
-                    rw.assert_local_file_pointer_now_at("Pathing T3 Entry Data 3", t3.offset_3)
-                rw.rw_obj_method(t3, t3.rw_data_3)
-                if t3.offset_4:
-                    rw.assert_local_file_pointer_now_at("Pathing T3 Entry Data 4", t3.offset_4)
-                rw.rw_obj_method(t3, t3.rw_data_4)
-        if self.t4_offset:
-            rw.assert_local_file_pointer_now_at("Pathing T4 Entry", self.t4_offset)
-            rw.rw_obj_method(self, self.rw_t4_data)
+        for edge in self.graph_edges:
+            if edge.param_list_offset:
+                rw.assert_local_file_pointer_now_at("Graph Edge Parameter List:", edge.param_list_offset)
+                rw.rw_obj_method(edge, edge.rw_param_ids)
 
         rw.align(rw.local_tell(), 0x10)
 
-    def rw_t1_data(self, rw):
-        if rw.mode() == "read":
-            self.t1_data = [Pathing_T1(self.context) for _ in range(self.t1_count)]
-        for t1 in self.t1_data:
-            rw.rw_obj(t1)
+        if self.graph_indices_offset:
+            rw.assert_local_file_pointer_now_at("Graph Indices:", self.graph_indices_offset)
+            rw.rw_obj_method(self, self.rw_graph_indices)
+            for graph_index in self.graph_indices:
+                if graph_index.node_list_offset:
+                    rw.assert_local_file_pointer_now_at("Graph Index Nodes:", graph_index.node_list_offset)
+                rw.rw_obj_method(graph_index, graph_index.rw_node_id_list)
+                if graph_index.edge_list_offset:
+                    rw.assert_local_file_pointer_now_at("Graph Index Edges:", graph_index.edge_list_offset)
+                rw.rw_obj_method(graph_index, graph_index.rw_edge_id_list)
+                if graph_index.start_node_offset:
+                    rw.assert_local_file_pointer_now_at("Graph Index Start Nodes:", graph_index.start_node_offset)
+                rw.rw_obj_method(graph_index, graph_index.rw_start_node_ids)
+                if graph_index.end_node_offset:
+                    rw.assert_local_file_pointer_now_at("Graph Index End Nodes:", graph_index.end_node_offset)
+                rw.rw_obj_method(graph_index, graph_index.rw_end_node_ids)
 
-    def rw_t2_data(self, rw):
-        if rw.mode() == "read":
-            self.param_set_references = [ParamSetReferences(self.context) for _ in range(self.t2_count)]
-        for t2 in self.param_set_references:
-            rw.rw_obj(t2)
+        if self.unused_nodes_offset:
+            rw.assert_local_file_pointer_now_at("Unused Graph Nodes:", self.unused_nodes_offset)
+            rw.rw_obj_method(self, self.rw_unused_nodes)
 
-    def rw_t3_data(self, rw):
-        if rw.mode() == "read":
-            self.t3_data = [Pathing_T3(self.context) for _ in range(self.t3_count)]
-        for t3 in self.t3_data:
-            rw.rw_obj(t3)
+        rw.align(rw.local_tell(), 0x10)
 
-    def rw_t4_data(self, rw):
-        self.unused_ids = rw.rw_uint32s(self.unused_ids, self.t4_count)
+    def rw_nodes(self, rw):
+        if rw.mode() == "read":
+            self.graph_nodes = [PathNode(self.context) for _ in range(self.node_count)]
+        for node in self.graph_nodes:
+            rw.rw_obj(node)
+
+    def rw_edges(self, rw):
+        if rw.mode() == "read":
+            self.graph_edges = [PathEdge(self.context) for _ in range(self.edge_count)]
+        for edge in self.graph_edges:
+            rw.rw_obj(edge)
+
+    def rw_graph_indices(self, rw):
+        if rw.mode() == "read":
+            self.graph_indices = [GraphIndex(self.context) for _ in range(self.graph_index_count)]
+        for graph_index in self.graph_indices:
+            rw.rw_obj(graph_index)
+
+    def rw_unused_nodes(self, rw):
+        self.unused_nodes = rw.rw_uint32s(self.unused_nodes, self.unused_node_count)
 
         
-class Pathing_T1(Serializable):
+class PathNode(Serializable):
     def __init__(self, endianness):
         super().__init__(endianness)
         
-        self.count_1 = 0
-        self.offset_1 = 0
-        self.count_2 = 0
-        self.offset_2 = 0
-        self.ID = 0
-        self.padding_0x14 = 0
-        self.padding_0x18 = 0
-        self.padding_0x1C = 0
+        self.next_edge_count       = 0
+        self.next_edge_list_offset = 0
+        self.prev_edge_count       = 0
+        self.prev_edge_list_offset = 0
+        self.node_param_id = 0
+        self.padding_0x14  = 0
+        self.padding_0x18  = 0
+        self.padding_0x1C  = 0
         
-        self.local_component_ids_1 = []
-        self.local_component_ids_2 = []
+        self.next_edges = []
+        self.prev_edges = []
 
     def read_write(self, rw):
-        self.count_1      = rw.rw_uint32(self.count_1)
-        self.offset_1     = rw.rw_pointer(self.offset_1)
-        self.count_2      = rw.rw_uint32(self.count_2)
-        self.offset_2     = rw.rw_pointer(self.offset_2)
+        self.next_edge_count       = rw.rw_uint32(self.next_edge_count)
+        self.next_edge_list_offset = rw.rw_pointer(self.next_edge_list_offset)
+        self.prev_edge_count       = rw.rw_uint32(self.prev_edge_count)
+        self.prev_edge_list_offset = rw.rw_pointer(self.prev_edge_list_offset)
         
-        self.ID           = rw.rw_uint32(self.ID)
-        self.padding_0x14 = rw.rw_pad32(self.padding_0x14)
-        self.padding_0x18 = rw.rw_pad32(self.padding_0x18)
-        self.padding_0x1C = rw.rw_pad32(self.padding_0x1C)
+        self.node_param_id = rw.rw_uint32(self.node_param_id)
+        self.padding_0x14  = rw.rw_pad32(self.padding_0x14)
+        self.padding_0x18  = rw.rw_pad32(self.padding_0x18)
+        self.padding_0x1C  = rw.rw_pad32(self.padding_0x1C)
         
         rw.assert_is_zero(self.padding_0x14)
         rw.assert_is_zero(self.padding_0x18)
         rw.assert_is_zero(self.padding_0x1C)
         
-    def rw_data_1(self, rw):
-        rw.assert_local_file_pointer_now_at("Pathing T1: LCI1: ", self.offset_1)
-        self.local_component_ids_1 = rw.rw_uint32s(self.local_component_ids_1, self.count_1)
+    def rw_next_edge_ids(self, rw):
+        rw.assert_local_file_pointer_now_at("PathNode: Previous Edges: ", self.next_edge_list_offset)
+        self.next_edges = rw.rw_uint32s(self.next_edges, self.next_edge_count)
         
-    def rw_data_2(self, rw):
-        rw.assert_local_file_pointer_now_at("Pathing T1: LCI2: ", self.offset_2)
-        self.local_component_ids_2 = rw.rw_uint32s(self.local_component_ids_2, self.count_2)
+    def rw_prev_edge_ids(self, rw):
+        rw.assert_local_file_pointer_now_at("PathNode: Next Edges:", self.prev_edge_list_offset)
+        self.prev_edges = rw.rw_uint32s(self.prev_edges, self.prev_edge_count)
         
 
-class ParamSetReferences(Serializable):
+class PathEdge(Serializable):
     def __init__(self, endianness):
         super().__init__(endianness)
         
-        self.table_component_id_1 = 0
-        self.table_component_id_2 = 0
-        self.count = 0
-        self.offset = 0
-        self.component_indices = []
+        self.prev_node     = 0
+        self.next_node         = 0
+        self.param_count       = 0
+        self.param_list_offset = 0
+        self.edge_param_ids    = []
     
     def read_write(self, rw):     
-        self.table_component_id_1 = rw.rw_uint32(self.table_component_id_1)
-        self.table_component_id_2 = rw.rw_uint32(self.table_component_id_2)
-        self.count                = rw.rw_uint32(self.count)
-        self.offset               = rw.rw_pointer(self.offset)
+        self.prev_node         = rw.rw_uint32(self.prev_node)
+        self.next_node         = rw.rw_uint32(self.next_node)
+        self.param_count       = rw.rw_uint32(self.param_count)
+        self.param_list_offset = rw.rw_pointer(self.param_list_offset)
         
-        rw.assert_equal(self.count, self.offset > 0)
+        rw.assert_equal(self.param_count, self.param_list_offset > 0)
 
-    def rw_data(self, rw):
-        rw.assert_local_file_pointer_now_at("Pathing T2: ", self.offset)
-        self.component_indices = rw.rw_uint32s(self.component_indices, self.count)
-        
+    def rw_param_ids(self, rw):
+        rw.assert_local_file_pointer_now_at("Path Edges: ", self.param_list_offset)
+        self.edge_param_ids = rw.rw_uint32s(self.edge_param_ids, self.param_count)
 
-class Pathing_T3(Serializable):
+
+class GraphIndex(Serializable):
     def __init__(self, endianness):
         super().__init__(endianness)
         
-        self.count_1  = 0
-        self.offset_1 = 0
-        self.count_2  = 0
-        self.offset_2 = 0
-        self.count_3  = 0
-        self.offset_3 = 0
-        self.count_4  = 0
-        self.offset_4 = 0
+        self.node_count        = 0
+        self.node_list_offset  = 0
+        self.edge_count        = 0
+        self.edge_list_offset  = 0
+        self.start_node_count  = 0
+        self.start_node_offset = 0
+        self.end_node_count    = 0
+        self.end_node_offset   = 0
         
-        self.unknown_0x20 = 0
+        self.is_loop      = 0
         self.padding_0x24 = 0
         self.padding_0x28 = 0
         self.padding_0x2C = 0
@@ -221,23 +222,23 @@ class Pathing_T3(Serializable):
         self.padding_0x38 = 0
         self.padding_0x3C = 0
         
-        self.table_component_id = None
-        self.local_component_id = None
-        self.data_3 = None
-        self.data_4 = None
+        self.node_id_list = None
+        self.edge_id_list = None
+        self.start_node_id_list = None
+        self.end_node_id_list = None
     
     def read_write(self, rw):
-        self.count_1  = rw.rw_uint32(self.count_1)
-        self.offset_1 = rw.rw_pointer(self.offset_1)
-        self.count_2  = rw.rw_uint32(self.count_2)
-        self.offset_2 = rw.rw_pointer(self.offset_2)
+        self.node_count       = rw.rw_uint32(self.node_count)
+        self.node_list_offset = rw.rw_pointer(self.node_list_offset)
+        self.edge_count       = rw.rw_uint32(self.edge_count)
+        self.edge_list_offset = rw.rw_pointer(self.edge_list_offset)
         
-        self.count_3  = rw.rw_uint32(self.count_3)
-        self.offset_3 = rw.rw_pointer(self.offset_3)
-        self.count_4  = rw.rw_uint32(self.count_4)
-        self.offset_4 = rw.rw_pointer(self.offset_4)
+        self.start_node_count  = rw.rw_uint32(self.start_node_count)
+        self.start_node_offset = rw.rw_pointer(self.start_node_offset)
+        self.end_node_count    = rw.rw_uint32(self.end_node_count)
+        self.end_node_offset   = rw.rw_pointer(self.end_node_offset)
         
-        self.unknown_0x20 = rw.rw_uint32(self.unknown_0x20) # Can be 0 or 1
+        self.is_loop      = rw.rw_uint32(self.is_loop)
         self.padding_0x24 = rw.rw_pad32(self.padding_0x24)
         self.padding_0x28 = rw.rw_pad32(self.padding_0x28)
         self.padding_0x2C = rw.rw_pad32(self.padding_0x2C)
@@ -256,15 +257,14 @@ class Pathing_T3(Serializable):
         rw.assert_is_zero(self.padding_0x38)
         rw.assert_is_zero(self.padding_0x3C)
 
-    def rw_data_1(self, rw):
-        self.table_component_id = rw.rw_uint32s(self.table_component_id, self.count_1)
+    def rw_node_id_list(self, rw):
+        self.node_id_list = rw.rw_uint32s(self.node_id_list, self.node_count)
         
-    def rw_data_2(self, rw):
-        self.local_component_id = rw.rw_uint32s(self.local_component_id, self.count_2)
+    def rw_edge_id_list(self, rw):
+        self.edge_id_list = rw.rw_uint32s(self.edge_id_list, self.edge_count)
         
-    def rw_data_3(self, rw):
-        self.data_3 = rw.rw_uint32s(self.data_3, self.count_3)
+    def rw_start_node_ids(self, rw):
+        self.start_node_id_list = rw.rw_uint32s(self.start_node_id_list, self.start_node_count)
         
-    def rw_data_4(self, rw):
-        self.data_4 = rw.rw_uint32s(self.data_4, self.count_4)
-        
+    def rw_end_node_ids(self, rw):
+        self.end_node_id_list = rw.rw_uint32s(self.end_node_id_list, self.end_node_count)
