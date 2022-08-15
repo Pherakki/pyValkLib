48c4cc043133c6a8eec18c4e4c03a19eece6ff9d
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-08-15 22:28:00 +0100

    Rewrite ENRS and CCRS such that they use the Compression sublibrary

--- containers/CCRS/CCRSReadWriter.py
+++ containers/CCRS/CCRSReadWriter.py
@@ -1,8 +1,8 @@
-from .Representations import CCRSRelRep, CCRSRelativeTemplateGenerator, CCRSTemplateComponent
-from .Representations import CCRSUnpackedRep, CCRSTemplatePack, CCRSTemplate, CCRSUnpackedTemplateComponents
+#from pyValkLib.utils.Compression.Stencilled import SCRelRep, SCRelativeTemplateGenerator, CCRSTemplateComponent
+#from pyValkLib.utils.Compression.Stencilled import SCUnpackedRep, SCTemplatePack, SCTemplate, CCRSUnpackedTemplateComponents
 
 from pyValkLib.serialisation.ValkSerializable import ValkSerializable32BH
-
+#from pyValkLib.utils.Compression.integers import compressInt, decompressInt
 
 class CCRSReadWriter(ValkSerializable32BH):
     FILETYPE = "CCRS"
@@ -25,114 +25,3 @@ class CCRSReadWriter(ValkSerializable32BH):
         
         rw.assert_is_zero(self.padding_0x20)
         self.data = rw.rw_uint8s(self.data, self.header.data_length - 0x10, endianness='<')
-        
-def pull_bytecode(ENRS_iter, byte_power, bytecode_value):
-    for _ in range((1 << byte_power) - 1):
-        elem = next(ENRS_iter)
-        bytecode_value <<= 8
-        bytecode_value |= elem
-        
-    return bytecode_value
-
-def decompressInt(ENRS_iter):
-    elem = next(ENRS_iter)
-    byte_power = elem >> 6
-    bytecode_value = elem & 0x3F
-    
-    return pull_bytecode(ENRS_iter, byte_power, bytecode_value)
-
-def compressInt(integer):
-    data = []
-    if integer < 2**6:
-        power_val = 0x00
-        
-        byte_1 = (integer & 0x3F) >> 0x00
-        
-        data.append(power_val | byte_1)
-    elif integer < 2**14:
-        power_val = 0x40
-        
-        byte_1 = (integer & 0x3F00) >> 0x08
-        byte_2 = (integer & 0x00FF) >> 0x00
-        
-        data.append(power_val | byte_1)
-        data.append(byte_2)
-    elif integer < 2**30:
-        power_val = 0x80
-        
-        byte_1 = (integer & 0x3F000000) >> 0x18
-        byte_2 = (integer & 0x00FF0000) >> 0x10
-        byte_3 = (integer & 0x0000FF00) >> 0x08
-        byte_4 = (integer & 0x000000FF) >> 0x00
-        
-        data.append(power_val | byte_1)
-        data.append(byte_2)
-        data.append(byte_3)
-        data.append(byte_4)
-    else:
-        raise ValueError(f"Int can be no larger than 2**30: {integer}.")
-    return data
-
-def decompressCCRS(num_groups, data):
-    CCRS_iter_data = iter(data)
-    out = CCRSRelRep()
-    
-    for _ in range(num_groups):
-        jump_from_previous_group = decompressInt(CCRS_iter_data)
-        num_sub_stencils = decompressInt(CCRS_iter_data)
-        stencil_size = decompressInt(CCRS_iter_data)
-        stencil_repetitions = decompressInt(CCRS_iter_data)
-
-        sub_stencil_defs = []
-        for j in range(num_sub_stencils):
-            sub_stencil_defs.append(CCRSTemplateComponent(decompressInt(CCRS_iter_data), 
-                                                          decompressInt(CCRS_iter_data), 
-                                                          decompressInt(CCRS_iter_data)))
-            
-        cgen = CCRSRelativeTemplateGenerator(jump_from_previous_group, stencil_size, stencil_repetitions,
-                                             sub_stencil_defs)
-        out.append(cgen)
-        
-    return out.to_abs_rep().to_unpacked_rep()
-
-def compressCCRS(ccrs_unpacked_rep):
-    out = []
-    
-    ccrs_rel_rep = ccrs_unpacked_rep.to_abs_rep().to_rel_rep()
-    for tg in ccrs_rel_rep:
-        out.extend(compressInt(tg.jump))
-        out.extend(compressInt(len(tg.subs)))
-        out.extend(compressInt(tg.stride))
-        out.extend(compressInt(tg.count))
-        
-        for comp in tg:
-            out.extend(compressInt(comp.stride))
-            out.extend(compressInt(comp.count))
-            out.extend(compressInt(comp.type))
-        
-    return out
-
-def toCCRSPackedRep(data):
-    out = CCRSUnpackedRep()
-    
-    for tpack_data in data:
-        templates = []
-        for template_data in tpack_data:
-            template = CCRSTemplate()
-            for comp_type, comp_data in template_data:
-                template.append(CCRSUnpackedTemplateComponents(comp_data, comp_type))
-            templates.append(template)
-        
-        # Shouldn't need to calculate this, should receive it..!!!
-        if len(templates) > 1:
-            stride = templates[1].get_first_offset() - templates[0].get_first_offset()
-        else:
-            stride = 1 # Wrong, but probably doesn't matter?
-            
-        tpack = CCRSTemplatePack(stride)
-        
-        for template in templates:
-            tpack.append(template)
-        out.append(tpack)
-
-    return out
--- containers/ENRS/ENRSReadWriter.py
+++ containers/ENRS/ENRSReadWriter.py
@@ -3,6 +3,11 @@ import array
 from pyValkLib.serialisation import Serializable
 from pyValkLib.serialisation.ValkSerializable import ValkSerializable32BH, Header32B
 
+#from pyValkLib.utils.Compression.Stencilled import SCRelRep, SCRelativeTemplateGenerator, ENRSTemplateComponent
+#from pyValkLib.utils.Compression.Stencilled import SCUnpackedRep, SCTemplatePack, SCTemplate, ENRSUnpackedTemplateComponents
+#from pyValkLib.utils.Compression.integers import compressInt, decompressInt
+#from pyValkLib.utils.Compression.integers import compressSubStencil, compressSubStencil_test, decompressSubStencil
+
 
 class ENRSReadWriter(ValkSerializable32BH):
     FILETYPE = "ENRS"
@@ -30,29 +35,6 @@ class ENRSReadWriter(ValkSerializable32BH):
 #################
 # DECOMPRESSION #
 #################
-def pull_bytecode(ENRS_iter, byte_power, bytecode_value):
-    for _ in range((1 << byte_power) - 1):
-        elem = next(ENRS_iter)
-        bytecode_value <<= 8
-        bytecode_value |= elem
-        
-    return bytecode_value
-
-def decompressInt(ENRS_iter):
-    elem = next(ENRS_iter)
-    byte_power = elem >> 6
-    bytecode_value = elem & 0x3F
-    
-    return pull_bytecode(ENRS_iter, byte_power, bytecode_value)
-
-
-def decompressSubStencilDef(ENRS_iter):
-    elem = next(ENRS_iter)
-    byte_power = elem >> 6
-    bytecode_value = elem & 0xF
-    array_byte_power = elem >> 4 & 3
-    
-    return pull_bytecode(ENRS_iter, byte_power, bytecode_value), array_byte_power
 
 def pull_bytecode_Bytes(ENRS_iter, byte_power, bytecode_value):
     out = []
@@ -84,186 +66,202 @@ class ENRSSubStencil:
     
     def __repr__(self):
         return f"<ENRSSubStencil><{self.itemsize}>{self.data}"
-
-def decompressENRS(num_groups, data):
-    offset = 0
-    offsets = []
     
-    ENRS_iter_data = iter(data)
-    for loop in range(num_groups):
-        # Decode the ENRS spec
-        jump_from_previous_stencil_group = decompressInt(ENRS_iter_data)
-        offset += jump_from_previous_stencil_group
-        
-        num_sub_stencils = decompressInt(ENRS_iter_data)
-        stencil_size = decompressInt(ENRS_iter_data)
-        stencil_repetitions = decompressInt(ENRS_iter_data)
-        working_offset = offset
-        sub_stencil_defs = []
-        for j in range(num_sub_stencils):    
-            sub_stencil_defs.append((*decompressSubStencilDef(ENRS_iter_data), decompressInt(ENRS_iter_data)))
+    def __iter__(self):
+        for elem in self.data:
+            yield elem
             
-        # Generate offsets
-        stencil_group = []
-        for i in range(stencil_repetitions):
-            saved_offset = working_offset + stencil_size
-            stencil = []
-            for j in range(num_sub_stencils):
-                jump_from_previous_substencil, elem_byte_power, elem_count = sub_stencil_defs[j]
-                working_offset += jump_from_previous_substencil
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.data[idx]
+    
+    def __setitem__(self, idx, value):
+        self.data[idx] = value
+
+
+# def decompressENRS_test(num_groups, data):
+#     ENRS_iter_data = iter(data)
+#     out = SCRelRep("ENRS")
+    
+#     for _ in range(num_groups):
+#         jump_from_previous_group = decompressInt(ENRS_iter_data)
+#         num_sub_stencils = decompressInt(ENRS_iter_data)
+#         stencil_size = decompressInt(ENRS_iter_data)
+#         stencil_repetitions = decompressInt(ENRS_iter_data)
+
+#         sub_stencil_defs = []
+#         for j in range(num_sub_stencils):
+#             jump, stride = decompressSubStencil(ENRS_iter_data)
+#             count = decompressInt(ENRS_iter_data)
+#             sub_stencil_defs.append(ENRSTemplateComponent(jump, count, stride))
+            
+#         cgen = SCRelativeTemplateGenerator(jump_from_previous_group, stencil_size, stencil_repetitions,
+#                                            sub_stencil_defs)
+#         out.append(cgen)
+        
+#     return out.to_abs_rep().to_unpacked_rep()
+
+# def decompressENRS(num_groups, data):
+#     offset = 0
+#     offsets = []
+    
+#     ENRS_iter_data = iter(data)
+#     for loop in range(num_groups):
+#         # Decode the ENRS spec
+#         jump_from_previous_stencil_group = decompressInt(ENRS_iter_data)
+#         offset += jump_from_previous_stencil_group
+        
+#         num_sub_stencils = decompressInt(ENRS_iter_data)
+#         stencil_size = decompressInt(ENRS_iter_data)
+#         stencil_repetitions = decompressInt(ENRS_iter_data)
+#         working_offset = offset
+#         sub_stencil_defs = []
+#         for j in range(num_sub_stencils):    
+#             sub_stencil_defs.append((*decompressSubStencil(ENRS_iter_data), decompressInt(ENRS_iter_data)))
+            
+#         # Generate offsets
+#         stencil_group = []
+#         for i in range(stencil_repetitions):
+#             saved_offset = working_offset + stencil_size
+#             stencil = []
+            
+#             do_print = working_offset == 99072
+#             if do_print:
+#                 print("NEW DECOMPRESS")
+            
+#             for j in range(num_sub_stencils):
+#                 jump_from_previous_substencil, elem_byte_power, elem_count = sub_stencil_defs[j]
+#                 working_offset += jump_from_previous_substencil
 
                     
-                diff = 2 << elem_byte_power
-                sub_stencil = []
-                for k in range(elem_count):
-                    sub_stencil.append(working_offset)
-                    working_offset += diff
+#                 diff = 2 << elem_byte_power
+#                 sub_stencil = []
+#                 for k in range(elem_count):
+#                     sub_stencil.append(working_offset)
+#                     working_offset += diff
 
-                sub_stencil = ENRSSubStencil(diff, sub_stencil)
-                stencil.append(sub_stencil)
-            stencil_group.append(stencil)
-            working_offset = saved_offset
-        offsets.append(stencil_group)
+#                 sub_stencil = ENRSSubStencil(diff, sub_stencil)
+#                 stencil.append(sub_stencil)
+                
+#                 if do_print:
+#                     print(jump_from_previous_substencil, elem_byte_power, elem_count)
+#                     print(sub_stencil)
+#             stencil_group.append(stencil)
+#             working_offset = saved_offset
+#         offsets.append(stencil_group)
         
-    return offsets
+#     return offsets
 
-def divideENRSBytes(num_groups, data):
-    bytes_out = []
+# def divideENRSBytes(num_groups, data):
+#     bytes_out = []
     
-    ENRS_iter_data = iter(data)
-    for loop in range(num_groups):
-        hdr = []
-        # Decode the ENRS spec
-        hdr.extend(decompressInt_Bytes(ENRS_iter_data))
+#     ENRS_iter_data = iter(data)
+#     for loop in range(num_groups):
+#         hdr = []
+#         # Decode the ENRS spec
+#         hdr.extend(decompressInt_Bytes(ENRS_iter_data))
         
-        num_sub_stencils_bytes = decompressInt_Bytes(ENRS_iter_data)
-        num_sub_stencils = decompressInt(iter(num_sub_stencils_bytes))
-        hdr.extend(num_sub_stencils_bytes)
-        hdr.extend(decompressInt_Bytes(ENRS_iter_data))
-        hdr.extend(decompressInt_Bytes(ENRS_iter_data))
+#         num_sub_stencils_bytes = decompressInt_Bytes(ENRS_iter_data)
+#         num_sub_stencils = decompressInt(iter(num_sub_stencils_bytes))
+#         hdr.extend(num_sub_stencils_bytes)
+#         hdr.extend(decompressInt_Bytes(ENRS_iter_data))
+#         hdr.extend(decompressInt_Bytes(ENRS_iter_data))
 
-        subs = []
-        for j in range(num_sub_stencils):    
-            subs.append([*decompressSubStencilDef_Bytes(ENRS_iter_data), decompressInt_Bytes(ENRS_iter_data)])
+#         subs = []
+#         for j in range(num_sub_stencils):    
+#             subs.append([*decompressSubStencilDef_Bytes(ENRS_iter_data), decompressInt_Bytes(ENRS_iter_data)])
 
-        bytes_out.append([hdr, subs])
+#         bytes_out.append([hdr, subs])
 
-    return bytes_out
-
-
-def compressInt(integer):
-    data = []
-    if integer < 2**6:
-        power_val = 0x00
-        
-        byte_1 = (integer & 0x3F) >> 0x00
-        
-        data.append(power_val | byte_1)
-    elif integer < 2**14:
-        power_val = 0x40
-        
-        byte_1 = (integer & 0x3F00) >> 0x08
-        byte_2 = (integer & 0x00FF) >> 0x00
-        
-        data.append(power_val | byte_1)
-        data.append(byte_2)
-    elif integer < 2**30:
-        power_val = 0x80
-        
-        byte_1 = (integer & 0x3F000000) >> 0x18
-        byte_2 = (integer & 0x00FF0000) >> 0x10
-        byte_3 = (integer & 0x0000FF00) >> 0x08
-        byte_4 = (integer & 0x000000FF) >> 0x00
-        
-        data.append(power_val | byte_1)
-        data.append(byte_2)
-        data.append(byte_3)
-        data.append(byte_4)
-    else:
-        raise ValueError(f"Int can be no larger than 2**30: {integer}.")
-    return data
-
-def compressSubStencil(starting_offset, diff):
-    data = []
-    elem_byte_power = (diff << 2) & 0x30
-    if starting_offset < 2**4:
-        power_val = 0x00
-        
-        byte_1 = (starting_offset & 0x0F) >> 0x00
-        
-        data.append(power_val | elem_byte_power | byte_1)
-    elif starting_offset < 2**12:
-        power_val = 0x40
-        
-        byte_1 = (starting_offset & 0x0F00) >> 0x08
-        byte_2 = (starting_offset & 0x00FF) >> 0x00
-        
-        data.append(power_val | elem_byte_power | byte_1)
-        data.append(byte_2)
-    elif starting_offset < 2**28:
-        power_val = 0x80
-        
-        byte_1 = (starting_offset & 0x0F000000) >> 0x18
-        byte_2 = (starting_offset & 0x00FF0000) >> 0x10
-        byte_3 = (starting_offset & 0x0000FF00) >> 0x08
-        byte_4 = (starting_offset & 0x000000FF) >> 0x00
-        
-        data.append(power_val | elem_byte_power | byte_1)
-        data.append(byte_2)
-        data.append(byte_3)
-        data.append(byte_4)
-    else:
-        raise ValueError(f"Offset can be no larger than 2**28: {diff}.")
-    return data
+#     return bytes_out
 
 
-###############
-# COMPRESSION #
-###############    
-def compressENRS(pointer_offsets):
-    data = []
-    prev_main_array_offset = 0
-    for idx, main_array in enumerate(pointer_offsets):
-        stencil = main_array[0]
+# ###############
+# # COMPRESSION #
+# ###############    
+
+# def compressENRS_test(enrs_unpacked_rep):
+#     out = []
+    
+#     enrs_rel_rep = enrs_unpacked_rep.to_abs_rep().to_rel_rep()
+#     for tg in enrs_rel_rep:
+#         out.extend(compressInt(tg.jump))
+#         out.extend(compressInt(len(tg.subs)))
+#         out.extend(compressInt(tg.stride))
+#         out.extend(compressInt(tg.count))
         
-        first_offset = main_array[0][0][0]
-        num_array_member_copies = len(main_array)
-        num_sub_stencils = len(main_array[0])
+#         for comp in tg:
+#             out.extend(compressSubStencil_test(comp.stride, comp.type))
+#             out.extend(compressInt(comp.count))
         
-        # Calculate the jump between stencils
-        # Surely this can be simplified?
-        if idx+1 == len(pointer_offsets):
-            #if len(main_array) == 1 and len(main_array[0]) == 1 and len(main_array[0][0]) == 1:
-            #     stencil_size = 1
-            if len(stencil) > 2:
-                stencil_size = stencil[1][0] - stencil[0][0]
-            else:
-                stencil_size = stencil[0].itemsize
-        else:
-            #is_contiguous = pointer_offsets[idx+1][0][0][0] == (main_array[-1][-1][-1] + main_array[-1][-1].itemsize)
-            #if len(main_array) == 1 and len(main_array[0]) == 1 and is_contiguous:
-            #     stencil_size = 1
-            #else:
-            offset_to_next_stencil_group = pointer_offsets[idx+1][0][0][0] - stencil[0][0]
-            stencil_size = offset_to_next_stencil_group // len(main_array)
+#             comp_1 = compressSubStencil_test(comp.stride, comp.type)
+#             comp_2 = compressInt(comp.count)
+#             decomp_1 = decompressSubStencil(iter(comp_1))
+#             decomp_2 = decompressInt(iter(comp_2))
+#             if comp.stride != decomp_1[0] or comp.type != decomp_1[1] or comp.count != decomp_2:
+#                 print(">>", comp.unpack())
+#                 print(">> BEFORE", comp.stride, comp.type, comp.count)
+#                 print(">> AFTER", *decomp_1, decomp_2)
+#                 assert 0
         
-        data.extend(compressInt(first_offset - prev_main_array_offset))
-        data.extend(compressInt(num_sub_stencils))
-        data.extend(compressInt(stencil_size))
-        data.extend(compressInt(num_array_member_copies))
+#     return out
+
+# def compressENRS(pointer_offsets):
+#     data = []
+#     prev_main_array_offset = 0
+#     for idx, main_array in enumerate(pointer_offsets):
+#         stencil = main_array[0]
         
-        prev_main_array_offset = first_offset
+#         first_offset = main_array[0][0][0]
+#         num_array_member_copies = len(main_array)
+#         num_sub_stencils = len(main_array[0])
         
-        previous_substencil_offset = stencil[0][0]
-        for j, sub_stencil in enumerate(stencil):
-            starting_offset = sub_stencil[0] - previous_substencil_offset
-            diff = sub_stencil.itemsize
+#         # Calculate the jump between stencils
+#         # Surely this can be simplified?
+#         if idx+1 == len(pointer_offsets):
+#             #if len(main_array) == 1 and len(main_array[0]) == 1 and len(main_array[0][0]) == 1:
+#             #     stencil_size = 1
+#             if len(stencil) > 2:
+#                 stencil_size = stencil[1][0] - stencil[0][0]
+#             else:
+#                 stencil_size = stencil[0].itemsize
+#         else:
+#             #is_contiguous = pointer_offsets[idx+1][0][0][0] == (main_array[-1][-1][-1] + main_array[-1][-1].itemsize)
+#             #if len(main_array) == 1 and len(main_array[0]) == 1 and is_contiguous:
+#             #     stencil_size = 1
+#             #else:
+#             offset_to_next_stencil_group = pointer_offsets[idx+1][0][0][0] - stencil[0][0]
+#             stencil_size = offset_to_next_stencil_group // len(main_array)
+        
+#         data.extend(compressInt(first_offset - prev_main_array_offset))
+#         data.extend(compressInt(num_sub_stencils))
+#         data.extend(compressInt(stencil_size))
+#         data.extend(compressInt(num_array_member_copies))
+        
+#         prev_main_array_offset = first_offset
+        
+#         do_print = stencil[0][0] == 99072
+#         if do_print:
+#             print("NEW COMPRESS")
+#             print(main_array)
+#         previous_substencil_offset = stencil[0][0]
+#         for j, sub_stencil in enumerate(stencil):
+#             starting_offset = sub_stencil[0] - previous_substencil_offset
+#             diff = sub_stencil.itemsize
             
-            sub_stencil_count = len(sub_stencil)
+#             sub_stencil_count = len(sub_stencil)
             
-            data.extend(compressSubStencil(starting_offset, diff))
-            data.extend(compressInt(sub_stencil_count))
+#             data.extend(compressSubStencil(starting_offset, diff))
+#             data.extend(compressInt(sub_stencil_count))
+#             if do_print:
+#                 print(">", previous_substencil_offset, sub_stencil[0])
+#                 print(">", starting_offset, diff, sub_stencil_count)
+#                 print(compressSubStencil(starting_offset, diff), decompressSubStencil(iter(compressSubStencil(starting_offset, diff))))
+#                 print(sub_stencil)
+#                 print(previous_substencil_offset, diff*sub_stencil_count)
             
-            previous_substencil_offset = sub_stencil[-1] + diff
+#             previous_substencil_offset = sub_stencil[-1] + diff
 
-    return data
+#     return data
