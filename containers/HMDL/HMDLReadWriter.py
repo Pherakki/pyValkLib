8864cb2db7180853383ed7e6c463945bb9549332
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-08-29 20:09:23 +0100

    Add HMDL ReadWriter

---
+++ containers/HMDL/HMDLReadWriter.py
@@ -0,0 +1,20 @@
+from pyValkLib.serialisation.ValkSerializable import ValkSerializable32BH
+
+from pyValkLib.containers.Metadata.EOFC.EOFCReadWriter import EOFCReadWriter
+
+
+class HMDLReadWriter(ValkSerializable32BH):
+    FILETYPE = "HMDL"
+    
+    def __init__(self, endianness=None):
+        super().__init__({}, endianness)
+        
+        self.header.flags = 0x10000000
+        self.EOFC = EOFCReadWriter("<")
+
+    
+    def read_write_contents(self, rw):
+        rw.assert_equal(self.header.flags, 0x10000000, lambda x: hex(x))
+
+    def __repr__(self):
+        return f"HMDL Object [{self.header.depth}] [0x{self.header.flags:0>8x}]. Contains KFMD."
