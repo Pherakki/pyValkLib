e7892dbaf1a2abfd4859f783225d5e380f5aaa3d
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-08-30 20:32:49 +0100

    Add KFMD

--- containers/HMDL/KFMD/KFMDReadWriter.py
+++ containers/HMDL/KFMD/KFMDReadWriter.py
@@ -1,24 +1,20 @@
 from pyValkLib.serialisation.ValkSerializable import ValkSerializable32BH
 
-from .KFMD.KFMDReadWriter import KFMDReadWriter
 from pyValkLib.containers.Metadata.EOFC.EOFCReadWriter import EOFCReadWriter
 
 
-class HMDLReadWriter(ValkSerializable32BH):
-    FILETYPE = "HMDL"
+class KFMDReadWriter(ValkSerializable32BH):
+    FILETYPE = "KFMD"
     
     def __init__(self, endianness=None):
         super().__init__({}, endianness)
         
         self.header.flags = 0x10000000
-        self.KFMD = KFMDReadWriter(endianness)
         self.EOFC = EOFCReadWriter("<")
 
-    def get_subcontainers(self):
-        return [self.KFMD, self.EOFC]
     
     def read_write_contents(self, rw):
         rw.assert_equal(self.header.flags, 0x10000000, lambda x: hex(x))
 
     def __repr__(self):
-        return f"HMDL Object [{self.header.depth}] [0x{self.header.flags:0>8x}]. Contains KFMD."
+        return f"KFMD Object [{self.header.depth}] [0x{self.header.flags:0>8x}]. Contains KFMS, KFMG, EOFC."
