0e17bc4fb6c1b0938334b3c5806ed524ecf1ed35
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-08-31 21:15:05 +0100

    Add __repr__

--- serialisation/PointerIndexableArray.py
+++ serialisation/PointerIndexableArray.py
@@ -46,6 +46,9 @@ class PointerIndexableArray(Serializable):
     def __len__(self):
         return len(self.data)
     
+    def __repr__(self):
+        return f"[Pointer Indexable Array]: {self.data}"
+    
 class PointerIndexableArrayInt8(PointerIndexableArray):
     def rw_element(self, rw, idx): self.data[idx] = rw.rw_int8(self.data[idx])
     
