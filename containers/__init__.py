97dc21fa93557c3e43f3db1f61c9228aed4600eb
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-09-18 17:06:32 +0100

    Restrict containers lookup to IZCA types

--- containers/__init__.py
+++ containers/__init__.py
@@ -1,32 +1,20 @@
 # from .IZCA.IZCAReadWriter import IZCAReadWriter
 # from .IZCA.CCOL.CCOLReadWriter import CCOLReadWriter
 # from .IZCA.HMDL.HMDLReadWriter import HMDLReadWriter
+from .HMOT.HMOTReadWriter import HMOTReadWriter
+from .HTEX.HTEXReadWriter import HTEXReadWriter
 # from .IZCA.HSHP.HSHPReadWriter import HSHPReadWriter
-# from .IZCA.HTEX.HTEXReadWriter import HTEXReadWriter
-# from .IZCA.MLX0.MLX0ReadWriter import MLX0ReadWriter
+from .IZCA.MLX0.MLX0ReadWriter import MLX0ReadWriter
 # from .IZCA.PJNT.PJNTReadWriter import PJNTReadWriter
 # from .IZCA.PACT.PACTReadWriter import PACTReadWriter
-from .MXEN.MXENReadWriter import MXENReadWriter
-from .MXEN.MXEC.MXECReadWriter import MXECReadWriter
-
-from .Metadata.CCRS.CCRSReadWriter import CCRSReadWriter
-from .Metadata.ENRS.ENRSReadWriter import ENRSReadWriter
-from .Metadata.EOFC.EOFCReadWriter import EOFCReadWriter
-from .Metadata.POF0.POF0ReadWriter import POF0ReadWriter
 
 containers = {
     # "CCOL": CCOLReadWriter,
-    "CCRS": CCRSReadWriter,
-    "ENRS": ENRSReadWriter,
-    "EOFC": EOFCReadWriter,
-    # "HMDL": HMDLReadWriter,
+    "HMDL": HMDLReadWriter,
+    "HMOT": HMOTReadWriter,
     # "HSHP": HSHPReadWriter,
-    # "HTEX": HTEXReadWriter,
-    # "IZCA": IZCAReadWriter,
-    # "MLX0": MLX0ReadWriter,
-    "MXEN": MXENReadWriter,
-    "MXEC": MXECReadWriter,
+    "HTEX": HTEXReadWriter,
+    "MLX0": MLX0ReadWriter,
     # "PACT": PACTReadWriter,
     # "PJNT": PJNTReadWriter,
-    "POF0": POF0ReadWriter
 }
