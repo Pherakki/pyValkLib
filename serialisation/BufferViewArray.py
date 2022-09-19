033b007028a3f3431dbe5620b3fdcba17e65e436
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-09-19 14:58:38 +0100

    Differentiate public accessors for the buffer and views

--- serialisation/BufferViewArray.py
+++ serialisation/BufferViewArray.py
@@ -13,6 +13,7 @@ class BufferViewArray(Serializable):
         self.views = None
         self.idx_anchor = None
         self.view_t = view_t
+        self.ptr_to_idx = {}
         
     def read_write(self, rw, buffer_size, view_offsets, view_args):
         rw.mark_new_contents_array()
@@ -21,10 +22,18 @@ class BufferViewArray(Serializable):
         self.buffer = memoryview(rw_op(rw, self.buffer, buffer_size))
         
         if rw.mode() == "read":
-            self.views = [self.view_t(self.buffer[self.ptr_to_idx(offset):], *args) 
+            self.views = [self.view_t(self.buffer[self.buffer_ptr_to_idx(offset):], *args) 
                           for offset, args 
                           in zip(view_offsets, view_args)]
-    
-    def ptr_to_idx(self, ptr):
+            self.view_ptr_to_idx = {v: k for k, v in enumerate(view_offsets)}
+            
+    def buffer_ptr_to_idx(self, ptr):
         return (ptr-self.idx_anchor)//self.view_t.element_size
         
+    def buffer_idx_to_ptr(self, idx):
+        return self.idx_anchor + self.view_t.element_size*idx
+    
+    def at_ptr(self, ptr):
+        idx = self.view_ptr_to_idx[ptr]
+        return self.views[idx]
+    
