ae0bb49e9e997825c2eca278fecdbb4c3c26e4db
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-10-05 22:39:56 +0100

    Fix bone IDs

--- containers/HMDL/KFMD/KFMS/Bone.py
+++ containers/HMDL/KFMD/KFMS/Bone.py
@@ -1,7 +1,7 @@
 from pyValkLib.serialisation.Serializable import Context, Serializable
 
 class Bone(Serializable):
-    __slots__ = ("ibpm_offset", "ID")
+    __slots__ = ("ibpm_offset", "unknown_0x04", "ID")
     
     def __init__(self, context=None):
         if context is None:
@@ -10,10 +10,13 @@ class Bone(Serializable):
         
         self.ibpm_offset = None
         self.ID = None
+        self.unknown_0x04 = 0
     
     def __repr__(self):
         return f"[Bone] {self.ID} {self.ibpm_offset}"
     
     def read_write(self, rw):
-        self.ibpm_offset = rw.rw_pointer(self.ibpm_offset)
-        self.ID          = rw.rw_uint32(self.ID)
+        self.ibpm_offset  = rw.rw_pointer(self.ibpm_offset)
+        self.unknown_0x04 = rw.rw_uint16(self.unknown_0x04)
+        self.ID           = rw.rw_uint16(self.ID)
+        rw.assert_is_zero(self.unknown_0x04)
