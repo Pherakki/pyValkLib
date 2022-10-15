da54b8781ced00784ba2d59884c545bc5094f4a0
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-10-15 17:22:09 +0100

    Make IZCAs skip HSPR files

--- containers/__init__.py
+++ containers/__init__.py
@@ -3,7 +3,7 @@ from .HMDL.HMDLReadWriter import HMDLReadWriter
 from .HMOT.HMOTReadWriter import HMOTReadWriter
 from .HTEX.HTEXReadWriter import HTEXReadWriter
 # from .IZCA.HSHP.HSHPReadWriter import HSHPReadWriter
-from .IZCA.HSPR.HSPRReadWriter import HSPRReadWriter
+from .IZCA.HSPR.HSPRReadWriter import HSPRSkipper
 from .IZCA.MLX0.MLX0ReadWriter import MLX0ReadWriter
 # from .IZCA.PJNT.PJNTReadWriter import PJNTReadWriter
 # from .IZCA.PACT.PACTReadWriter import PACTReadWriter
@@ -14,7 +14,7 @@ containers = {
     "HMDL": HMDLReadWriter,
     "HMOT": HMOTReadWriter,
     # "HSHP": HSHPReadWriter,
-    "HSPR": HSPRReadWriter,
+    "HSPR": HSPRSkipper,
     "HTEX": HTEXReadWriter,
     "MLX0": MLX0ReadWriter,
     # "PACT": PACTReadWriter,
