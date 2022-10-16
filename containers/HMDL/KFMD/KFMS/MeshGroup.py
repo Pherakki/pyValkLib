0b317265c875517b51276c1d8241a8acd77392bc
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-10-16 15:30:01 +0100

    Fix __repr__

--- containers/HMDL/KFMD/KFMS/MeshGroup.py
+++ containers/HMDL/KFMD/KFMS/MeshGroup.py
@@ -17,7 +17,7 @@ class MeshGroup(Serializable):
         self.vertex_count       = None
         
     def __repr__(self):
-        return f"[MeshGroup {self.is_root} {self.ID}] {self.material_offset} {self.unknown_0x0A} {self.mesh_count} {self.mesh_list_offset} {self.kfmg_vertex_offset} {self.vertex_count}"
+        return f"[MeshGroup {self.is_root} {self.ID}] {self.material_offset} {self.unknown_0x0A} {self.mesh_count} {self.meshes_offset} {self.vertex_offset} {self.vertex_count}"
         
     def read_write(self, rw):
         self.ID                 = rw.rw_uint32(self.ID)
