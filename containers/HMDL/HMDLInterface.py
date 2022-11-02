8849a3468fe8a7e5581578f8afb5f8a12659a2ec
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-11-02 21:53:50 +0000

    Dumb down the HMDL interface temporarily so it can definitely reproduce the binary

--- containers/HMDL/HMDLInterface.py
+++ containers/HMDL/HMDLInterface.py
@@ -4,6 +4,7 @@ from .KFMD.KFMS.SceneNode import SceneNodeBinary
 from .KFMD.KFMS.MeshDefinition import MeshDefinitionBinary
 from .KFMD.KFMS.Skeleton import SkeletonBinary
 from .KFMD.KFMS.Bone import BoneBinary
+from .KFMD.KFMS.BoundingBox import BoundingBoxBinary
 from .KFMD.KFMS.MeshGroup import MeshGroupBinary
 from .KFMD.KFMS.Mesh import MeshBinary
 from .KFMD.KFMS.Material import MaterialBinary
@@ -17,25 +18,32 @@ class KFMDInterface:
         # Replace a SceneNode tree with an Armature where appropriate?
         # Can SceneNodes be mixed with Bones?
         # Meshes can be placed as leaf nodes of bones...
-        self.flags = None
+        self.flags           = None
         self.unknown_count_4 = None
-        self.unknown_0x64 = None
-        self.unknown_0x70 = None
-        self.unknown_0x74 = None
-        self.unknown_0x80 = None
-        self.scene_nodes = []
-        self.unknown_1s  = []
-        self.unknown_2s  = []
-        self.unknown_3s  = []
-        self.mesh_defs   = []
-        self.skeletons   = []
-        self.bones       = []
-        self.meshes      = []
-        self.materials   = []
-        self.textures    = []
+        self.unknown_0x64    = None
+        self.unknown_0x70    = None
+        self.unknown_0x74    = None
+        self.unknown_0x80    = None
+    
+        self.scene_node_flags = []
+        self.scene_nodes     = []
+        self.scene_node_transforms = []
+        self.unknown_1s      = []
+        self.unknown_2s      = []
+        self.unknown_3s      = []
+        self.mesh_defs       = []
+        self.bounding_boxes  = [] # Should be calculable
+        self.skeletons       = []
+        self.bones           = []
+        self.ibpms           = [] # Should be calculable
+        self.mesh_groups     = []
+        self.materials       = []
+        self.meshes          = []
+        self.vertex_groups   = [] # Should be calculable
+        self.textures        = []
         self.unknown_indices = None
         self.unknown_objects = []
-        
+
     @classmethod
     def from_binary(cls, KFMD_binary):
         binary = KFMD_binary.KFMS
@@ -49,21 +57,75 @@ class KFMDInterface:
         instance.unknown_0x70 = binary.unknown_0x70
         instance.unknown_0x74 = binary.unknown_0x74
         instance.unknown_0x80 = binary.unknown_0x80
-        instance.scene_nodes = [SceneNodeInterface.from_binary(sno, flag, transform, binary.mesh_groups, binary.skeletons, binary.bones)
-                                for flag, sno, transform in zip(binary.scene_node_flags, binary.scene_nodes, binary.scene_node_transforms)]
-        instance.unknown_1s = binary.unknown_chunk_1
-        instance.unknown_2s = binary.unknown_chunk_2
-        instance.unknown_3s = binary.unknown_chunk_3
-        instance.skeletons = [SkeletonInterface.from_binary(skel) for skel in binary.skeletons]
-        instance.bones     = [BoneInterface.from_binary(bone) for bone in binary.bones]
-        instance.meshes    = [MeshInterface.from_binary(mesh, [], [], binary.vertex_groups) for mesh in binary.meshes]
-        instance.materials = [MaterialInterface.from_binary(mat, binary.textures) for mat in binary.materials]
-        instance.textures  = [TextureInterface.from_binary(tex) for tex in binary.textures]
-        instance.unknown_indices = UnknownIndicesInterface.from_binary(binary.unknown_indices)
-        instance.unknown_objects = [o for o in binary.unknown_objects]
+        
+        # Calculable header variables
+        instance.model_height                 = binary.model_height
+        instance.scene_node_count             = binary.scene_node_count
+        instance.scene_nodes_offset           = binary.scene_nodes_offset
+        instance.scene_node_flags_offset      = binary.scene_node_flags_offset
+        instance.scene_node_transforms_offset = binary.scene_node_transforms_offset
+        instance.material_count               = binary.material_count
+        instance.materials_offset             = binary.materials_offset
+        instance.mesh_group_count             = binary.mesh_group_count
+        instance.mesh_groups_offset           = binary.mesh_groups_offset
+        instance.mesh_count                   = binary.mesh_count
+        instance.meshes_offset                = binary.meshes_offset
+        instance.unknown_obj_count            = binary.unknown_obj_count
+        instance.unknown_objs_offset          = binary.unknown_objs_offset
+        instance.texture_count                = binary.texture_count
+        instance.textures_offset              = binary.textures_offset
+        instance.unknown_count_1              = binary.unknown_count_1
+        instance.unknown_offset_1             = binary.unknown_offset_1
+        instance.unknown_count_2              = binary.unknown_count_2
+        instance.unknown_offset_2             = binary.unknown_offset_2
+        instance.unknown_count_3              = binary.unknown_count_3
+        instance.unknown_offset_3             = binary.unknown_offset_3
+        instance.mesh_defs_count              = binary.mesh_defs_count
+        instance.mesh_defs_offset             = binary.mesh_defs_offset
+        
+        instance.bone_count                   = binary.bone_count
+        instance.unknown_indices_offset       = binary.unknown_indices_offset
+
+        # Contents
+        instance.scene_node_flags = binary.scene_node_flags
+        instance.scene_nodes     = binary.scene_nodes
+        instance.scene_node_transforms = binary.scene_node_transforms
+        instance.unknown_1s      = binary.unknown_chunk_1
+        instance.unknown_2s      = binary.unknown_chunk_2
+        instance.unknown_3s      = binary.unknown_chunk_3
+        instance.mesh_defs       = binary.mesh_definitions
+        instance.bounding_boxes  = binary.bounding_boxes
+        instance.skeletons       = binary.skeletons
+        instance.bones           = binary.bones
+        instance.ibpms           = binary.bone_ibpms
+        instance.mesh_groups     = binary.mesh_groups
+        instance.materials       = binary.materials
+        instance.meshes          = binary.meshes
+        instance.vertex_groups   = binary.vertex_groups
+        instance.textures        = binary.textures
+        instance.unknown_indices = binary.unknown_indices
+        instance.unknown_objects = binary.unknown_objects
+        
+
+        # instance.scene_nodes = [SceneNodeInterface.from_binary(sno, flag, transform, binary.mesh_groups, binary.skeletons, binary.bones)
+        #                         for flag, sno, transform in zip(binary.scene_node_flags, binary.scene_nodes, binary.scene_node_transforms)]
+        # instance.unknown_1s = binary.unknown_chunk_1
+        # instance.unknown_2s = binary.unknown_chunk_2
+        # instance.unknown_3s = binary.unknown_chunk_3
+        # instance.skeletons  = [SkeletonInterface.from_binary(skel) for skel in binary.skeletons]
+        # instance.bones      = [BoneInterface.from_binary(bone) for bone in binary.bones]
+        # instance.bounding_boxes = [BoundingBoxInterface.from_binary(bbox) for bbox in binary.bounding_boxes]
+        # instance.ibpms      = binary.bone_ibpms
+        # instance.meshes     = [MeshInterface.from_binary(mesh, [], [], binary.vertex_groups) for mesh in binary.meshes]
+        # instance.materials  = [MaterialInterface.from_binary(mat, binary.textures) for mat in binary.materials]
+        # instance.vertex_groups = binary.vertex_groups
+        # instance.textures   = [TextureInterface.from_binary(tex) for tex in binary.textures]
+        # print(binary.unknown_indices)
+        # instance.unknown_indices = UnknownIndicesInterface.from_binary(binary.unknown_indices)
+        # instance.unknown_objects = [o for o in binary.unknown_objects]
         return instance
         
-    def to_binary(self, endianness):
+    def to_binary(self, endianness, depth, POF0, ENRS, CCRS, MTXS):
         KFMD_binary = KFMDReadWriter(endianness)
         
         # First dump KFMG
@@ -72,21 +134,9 @@ class KFMDInterface:
         # Dump KFMS
         binary = KFMD_binary.KFMS
         ctx = binary.context
+
         
-        binary.scene_node_flags,
-        binary.scene_nodes,
-        binary.scene_node_transforms = zip(sni.to_binary(ctx) for sni in self.scene_nodes)
-        binary.unknown_chunk_1 = self.unknown_1s
-        binary.unknown_chunk_2 = self.unknown_2s
-        binary.unknown_chunk_3 = self.unknown_3s
-        binary.skeletons.data  = [si.to_binary(ctx) for si in self.skeletons]
-        binary.bones.data      = [bi.to_binary(ctx) for bi in self.bones]
-        binary.meshes.data     = [mi.to_binary(ctx) for mi in self.meshes] # Need a PIA constructor that auto-gens pointers...
-        binary.materials.data  = [mi.to_binary(ctx) for mi in self.materials]
-        binary.textures.data   = [ti.to_binary(ctx) for ti in self.textures]
-        binary.unknown_indices = UnknownIndicesInterface.to_binary(ctx, self.unknown_indices)
-        binary.unknown_objects = [ui.to_binary(ctx) for ui in self.unknown_objects]
-        
+        # Unknown variables
         binary.flags             = self.flags
         binary.unknown_count_4   = self.unknown_count_4
         binary.unknown_0x64      = self.unknown_0x64
@@ -94,43 +144,110 @@ class KFMDInterface:
         binary.unknown_0x70      = self.unknown_0x70
         binary.unknown_0x74      = self.unknown_0x74
         binary.unknown_0x80      = self.unknown_0x80
-        binary.scene_node_count  = len(self.scene_nodes)
-        binary.bone_count        = len(self.bones)
         
-        binary.material_count    = len(self.materials)
-        binary.mesh_group_count  = len(self.mesh_groups)
-        binary.mesh_count        = len(self.meshes)
-        binary.unknown_obj_count = len(self.unknown_objects)
-        binary.texture_count     = len(self.textures)
-        binary.unknown_count_1   = len(self.unknown_chunk_1)
-        binary.unknown_count_2   = len(self.unknown_chunk_2)
-        binary.unknown_count_3   = len(self.unknown_chunk_3)
-        binary.mesh_defs_count   = len(self.mesh_defs)
+        # Calculable header variables
+        binary.model_height                 = self.model_height
+        binary.scene_node_count             = self.scene_node_count
+        binary.scene_nodes_offset           = self.scene_nodes_offset
+        binary.scene_node_flags_offset      = self.scene_node_flags_offset
+        binary.scene_node_transforms_offset = self.scene_node_transforms_offset
+        binary.material_count               = self.material_count
+        binary.materials_offset             = self.materials_offset
+        binary.mesh_group_count             = self.mesh_group_count
+        binary.mesh_groups_offset           = self.mesh_groups_offset
+        binary.mesh_count                   = self.mesh_count
+        binary.meshes_offset                = self.meshes_offset
+        binary.unknown_obj_count            = self.unknown_obj_count
+        binary.unknown_objs_offset          = self.unknown_objs_offset
+        binary.texture_count                = self.texture_count
+        binary.textures_offset              = self.textures_offset
+        binary.unknown_count_1              = self.unknown_count_1
+        binary.unknown_offset_1             = self.unknown_offset_1
+        binary.unknown_count_2              = self.unknown_count_2
+        binary.unknown_offset_2             = self.unknown_offset_2
+        binary.unknown_count_3              = self.unknown_count_3
+        binary.unknown_offset_3             = self.unknown_offset_3
+        binary.mesh_defs_count              = self.mesh_defs_count
+        binary.mesh_defs_offset             = self.mesh_defs_offset
         
-        # Now construct derived objects
+        binary.bone_count                   = self.bone_count
+        binary.unknown_indices_offset       = self.unknown_indices_offset
+
+        # Contents
+        binary.scene_node_flags = self.scene_node_flags
+        binary.scene_nodes      = self.scene_nodes
+        binary.scene_node_transforms = self.scene_node_transforms
+        binary.unknown_chunk_1  = self.unknown_1s
+        binary.unknown_chunk_2  = self.unknown_2s
+        binary.unknown_chunk_3  = self.unknown_3s
+        binary.mesh_definitions = self.mesh_defs
+        binary.bounding_boxes   = self.bounding_boxes
+        binary.skeletons        = self.skeletons
+        binary.bones            = self.bones
+        binary.bone_ibpms       = self.ibpms
+        binary.mesh_groups      = self.mesh_groups
+        binary.materials        = self.materials
+        binary.meshes           = self.meshes
+        binary.vertex_groups    = self.vertex_groups
+        binary.textures         = self.textures
+        binary.unknown_indices  = self.unknown_indices
+        binary.unknown_objects  = self.unknown_objects
+        
+        
+        # binary.scene_node_count  = len(self.scene_nodes)
+        # binary.bone_count        = len(self.bones)
+        
+        # binary.material_count    = len(self.materials)
+        # binary.mesh_group_count  = len(self.mesh_groups)
+        # binary.mesh_count        = len(self.meshes)
+        # binary.unknown_obj_count = len(self.unknown_objects)
+        # binary.texture_count     = len(self.textures)
+        # binary.unknown_count_1   = len(self.unknown_chunk_1)
+        # binary.unknown_count_2   = len(self.unknown_chunk_2)
+        # binary.unknown_count_3   = len(self.unknown_chunk_3)
+        # binary.mesh_defs_count   = len(self.mesh_defs)
+        
+        # # Fill in data
+        
+        # binary.scene_node_flags,
+        # binary.scene_nodes,
+        # binary.scene_node_transforms = zip(sni.to_binary(ctx) for sni in self.scene_nodes)
+        # binary.unknown_chunk_1 = self.unknown_1s
+        # binary.unknown_chunk_2 = self.unknown_2s
+        # binary.unknown_chunk_3 = self.unknown_3s
+        # binary.skeletons.data  = [si.to_binary(ctx) for si in self.skeletons]
+        # binary.bones.data      = [bi.to_binary(ctx) for bi in self.bones]
+        # binary.meshes.data     = [mi.to_binary(ctx) for mi in self.meshes] # Need a PIA constructor that auto-gens pointers...
+        # binary.materials.data  = [mi.to_binary(ctx) for mi in self.materials]
+        # binary.textures.data   = [ti.to_binary(ctx) for ti in self.textures]
+        # binary.unknown_indices = UnknownIndicesInterface.to_binary(ctx, self.unknown_indices)
+        # binary.unknown_objects = [ui.to_binary(ctx) for ui in self.unknown_objects]
+        
+        # # Now construct derived objects
+        
         
         # Now fill in offsets...
         ot = OffsetTracker()
         binary.header.read_write(ot)
         binary.rw_fileinfo(ot)
         
-        binary.scene_node_flags_offset = ot.local_tell()
+        #binary.scene_node_flags_offset = ot.local_tell()
         binary.rw_scene_node_flags(ot)
         
-        binary.scene_nodes_offset = ot.local_tell()
+        #binary.scene_nodes_offset = ot.local_tell()
         binary.rw_scene_nodes(ot)
         
-        binary.scene_node_transforms_offset = ot.local_tell()
+        #binary.scene_node_transforms_offset = ot.local_tell()
         binary.rw_scene_node_transforms(ot)
         
-        binary.unknown_offset_1 = ot.local_tell()
+        #binary.unknown_offset_1 = ot.local_tell()
         binary.rw_unknown_chunk_1(ot)
-        binary.unknown_offset_2 = ot.local_tell()
+        #binary.unknown_offset_2 = ot.local_tell()
         binary.rw_unknown_chunk_2(ot)
-        binary.unknown_offset_3 = ot.local_tell()
+        #binary.unknown_offset_3 = ot.local_tell()
         binary.rw_unknown_chunk_3(ot)
         
-        binary.mesh_defs_offset = ot.local_tell()
+       # binary.mesh_defs_offset = ot.local_tell()
         binary.rw_mesh_definitions(ot)
         
         # Construct bounding boxes...!
@@ -139,28 +256,45 @@ class KFMDInterface:
         binary.rw_bones(ot)
         binary.rw_bone_ibpms(ot)
         
-        binary.mesh_groups_offset = ot.local_tell()
+        #binary.mesh_groups_offset = ot.local_tell()
         binary.rw_mesh_groups(ot)
         
-        binary.materials_offset = ot.local_tell()
+        #binary.materials_offset = ot.local_tell()
         binary.rw_materials(ot)
         
-        binary.meshes_offset = ot.local_tell()
+        #binary.meshes_offset = ot.local_tell()
         binary.rw_meshes(ot)
         
         # Construct vertex groups...
         binary.rw_vertex_groups(ot)
         
-        binary.textures_offset = ot.local_tell()
+        #binary.textures_offset = ot.local_tell()
         binary.rw_textures(ot)
         
-        binary.unknown_indices_offset = ot.local_tell()
+        #binary.unknown_indices_offset = ot.local_tell()
         binary.rw_unknown_indices(ot)
         
-        binary.unknown_objs_offset = ot.local_tell()
+        #binary.unknown_objs_offset = ot.local_tell()
         binary.rw_unknown_objects(ot)
         
-        binary.model_height = None
+        # Deal with Metadata
+        binary.header.data_length = ot.local_tell() - binary.header.header_length
+        binary.header.depth = depth + 1
+        
+        binary.POF0 = POF0
+        binary.ENRS = ENRS
+        binary.CCRS = CCRS
+        binary.MTXS = MTXS
+        binary.EOFC.header.depth = binary.header.depth + 1
+        ot.rw_obj(binary.POF0)
+        ot.rw_obj(binary.ENRS)
+        ot.rw_obj(binary.CCRS)
+        ot.rw_obj(binary.MTXS)
+        ot.rw_obj(binary.EOFC)
+        
+        binary.header.contents_length = ot.local_tell() - binary.header.header_length
+        
+        print(binary.header)
         
         return binary
     
@@ -271,6 +405,22 @@ class BoneInterface:
     def to_binary(self, context):
         binary = BoneBinary(context)
         binary.unknown_0x04 = self.unknown_0x04
+        return binary
+
+class BoundingBoxInterface:
+    def __init__(self):
+        self.vertices = []
+        
+    @classmethod
+    def from_binary(cls, binary):
+        instance = cls()
+        instance.vertices = binary.vertices
+        return instance
+    
+    def to_binary(self, context):
+        binary = BoundingBoxBinary(context)
+        binary.vertex_count = len(self.vertices)
+        binary.vertices = self.vertices
         return binary
 
 class MeshGroupInterface:
